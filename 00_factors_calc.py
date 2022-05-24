28099a7157d91063e3d6bd9cad4e18f291745e64
simeonk1234 <101219115+simeonk1234@users.noreply.github.com>
2022-05-24 12:20:50 +1200

    Add files via upload

---
+++ 00_factors_calc.py
@@ -0,0 +1,35 @@
+# Main routine goes here
+
+# Heading
+
+statement_generator("Factors Calculator", "-")
+
+# Display instructions if user has not used the programme before
+first_time = input("Press <enter> to see the instructions or any key to continue")
+
+if first_time == "":
+    instructions()
+
+# Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+
+    comment = ""
+
+    # ask user for number to be factored...
+    var_to_factor = num_check("Number? ")
+
+    if var_to_factor != 1:
+        factor_list = get_factors(var_to_factor)
+    else:
+        factor_list = ""
+        comment = "One is UNITY!  It only has one factor.  Itself :)"
+
+    # comments for squares / primes
+    if len(factor_list) == 2:
+        comment = "{} is a prime number.".format(var_to_factor)
+    elif len(factor_list) % 2 == 1:
+        comment = "{} is a perfect square".format(var_to_factor)
+
+
+
