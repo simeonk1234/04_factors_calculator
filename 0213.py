7cfe702c67c0da4164e02c1576797e5d41323b24
simeonk1234 <101219115+simeonk1234@users.noreply.github.com>
2022-06-13 12:45:30 +1200

    Delete 0213.py

--- 0213.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_factors (num):
-    print('The factors of', num, 'are:')
-    for i in range(1, num+1):
-        if(num % i)  == 0:
-            print(i, end=' ')
-            
-            return ""
